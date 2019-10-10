22622220c1be1c29cf895fb0f26b85b612aae7b1
Olumide Okedusi <odokedusi@sidmach.com>
2019-10-10 12:52:24 +0100

    Initial commit

---
+++ src/components/AppNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>&nbsp;
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  padding: 15px 0px;
+}
+nav a {
+  margin: 0px 30px;
+}
+</style>
